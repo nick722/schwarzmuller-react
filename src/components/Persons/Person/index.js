0107077cc24d20e3c93847d069c900d15e9722b6
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-15 20:45:17 +0300

    083 Converting Stateless to Stateful Components

--- src/components/Persons/Person/index.js
+++ src/components/Persons/Person/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './Person.css';
 
-const Person = ({name, age, children, click, change}) => {
+class Person extends Component {
+    render() {
+        const {name, age, children, click, change} = this.props;
 
-    return (
-        <div className="Person">
-            <p onClick={click}>I'm {name} and I am {age} years old!</p>
-            <p>{children}</p>
-            <input
-                type="text"
-                onChange={change}
-                value={name}
-            />
-        </div>
-    );
-};
+        return (
+            <div className="Person">
+                <p onClick={click}>I'm {name} and I am {age} years old!</p>
+                <p>{children}</p>
+                <input
+                    type="text"
+                    onChange={change}
+                    value={name}
+                />
+            </div>
+        );
+    };
+}
 
 export default Person;
--- src/components/Persons/index.js
+++ src/components/Persons/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Person from './Person';
 
-const Persons = ({persons, deletePerson, changeName}) =>
-    persons.map((person, index) => (
-        <Person
-            click={() => deletePerson(index)}
-            key={person.id}
-            name={person.name}
-            age={person.age}
-            change={(event) => changeName(event, person.id)}
-        />
-    ));
+class Persons extends Component {
+    render() {
+        const {persons, deletePerson, changeName} = this.props;
+        return persons.map((person, index) => (
+            <Person
+                click={() => deletePerson(index)}
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                change={(event) => changeName(event, person.id)}
+            />
+        ));
+    }
+}
 
 export default Persons;
