038e2a701c121586dadab891ce7eed05a4b8753a
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-15 09:43:12 +0300

    080 Splitting an App Into Components

--- src/components/Cockpit/index.js
+++ src/components/Cockpit/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './Cockpit.css';
 
 const Cockpit = ({persons, style, showHidePersons}) => {
-    //showPersons,
 
     const classes = [];
 
@@ -14,12 +13,8 @@ const Cockpit = ({persons, style, showHidePersons}) => {
         classes.push('bold');
     }
 
-    // let btnClass = '';
-    // btnClass = 'red';
-
     return (
         <div
-            // className="Cockpit"
         >
             <h1>Hi, I'm a YN Todo React App</h1>
             <p
