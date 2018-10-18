c84c308a488e1f88c96004411e05b0ba96591d46
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-18 21:27:24 +0300

    116 Starting the Burger Component

--- src/components/Burger/BurgerIngredient/index.js
+++ src/components/Burger/BurgerIngredient/index.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import './BurgerIngredient.css';
+import classes from './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
-// = ({ type }) => {
     render () {
         let { type } = this.props;
         let ingredient = null;
 
         switch (type) {
             case ('bread-bottom'):
-                ingredient = <div className="BreadBottom"></div>;
+                ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case ('bread-top'):
                 ingredient = (
-                    <div className="BreadTop">
-                        <div className="Seeds1"></div>
-                        <div className="Seeds2"></div>
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
                     </div>
                 );
                 break;
             case ('meat'):
-                ingredient = <div className="Meat"></div>;
+                ingredient = <div className={classes.Meat}></div>;
                 break;
             case ('cheese'):
-                ingredient = <div className="Cheese"></div>;
+                ingredient = <div className={classes.Cheese}></div>;
                 break;
             case ('bacon'):
-                ingredient = <div className="Bacon"></div>;
+                ingredient = <div className={classes.Bacon}></div>;
                 break;
             case ('salad'):
-                ingredient = <div className="Salad"></div>;
+                ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;
