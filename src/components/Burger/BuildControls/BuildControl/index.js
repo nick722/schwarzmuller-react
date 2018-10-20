e7f0d7bfae995add1cddfbeb3c314b2e01920c98
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-20 12:23:53 +0300

    121 Connecting State to Build Controls

--- src/components/Burger/BuildControls/BuildControl/index.js
+++ src/components/Burger/BuildControls/BuildControl/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import classes from './BuildControl.css';
 
-const BuildControl = ({ label }) => (
+const BuildControl = ({ label, add }) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{label}</div>
         <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button
+            className={classes.More}
+            onClick={add}
+        >
+            More</button>
     </div>
 );
 
--- src/components/Burger/BuildControls/index.js
+++ src/components/Burger/BuildControls/index.js
@@ -10,12 +10,13 @@ const controls = [
     { labeL: 'Meat', type: 'meat' },
 ];
 
-const BuildControls = (props) => (
+const BuildControls = ({ ingredientAdd }) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
                 label={ctrl.label}
+                add={() => ingredientAdd(ctrl.type)}
             />
         ))}
     </div>
