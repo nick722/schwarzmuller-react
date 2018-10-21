bea8dc5930769f7f27e69c560a3c254e80a01f87
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-21 18:33:50 +0300

    149 Understanding our Project and Introducing Axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 
-import BurgerBuilder from './containers/BurgerBuilder';
-
-import Layout from './hoc/Layout';
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <BurgerBuilder/>
-                </Layout>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
 }
 
 export default App;
