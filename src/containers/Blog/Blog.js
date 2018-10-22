c440d8765fea4c77bd3a3215ccb89fbc72734cbb
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-10-22 11:53:14 +0300

    140. Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,49 +9,58 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
-    }
+        selectedPostId: null,
+        error: false
+    };
 
     componentDidMount() {
         axious.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
-                   return {
-                       ...post,
-                       author: 'Piper'
-                   }
+                    return {
+                        ...post,
+                        author: 'Piper'
+                    };
                 });
 
-                this.setState({posts: updatedPosts});
+                this.setState({ posts: updatedPosts });
                 // console.log(response);
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({ error: true });
             });
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
+        this.setState({ selectedPostId: id });
+    };
 
-    render () {
-        const posts = this.state.posts.map(post => {
-           return <Post
-               key={post.id}
-               title={post.title}
-               author={post.author}
-               clicked={() => this.postSelectedHandler(post.id)}
-           />;
-        });
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />;
+            });
+        }
 
         return (
             <div>
                 <section className="Posts">
-                   {posts}
+                    {posts}
                 </section>
                 <section>
                     <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
