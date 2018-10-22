f5d2dbdcab8859124aa5a3fc67044543d683b914
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-10-22 12:47:10 +0300

    143. Creating and Using Axios Instances

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axious from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -14,7 +15,7 @@ class Blog extends Component {
     };
 
     componentDidMount() {
-        axious.get('/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
