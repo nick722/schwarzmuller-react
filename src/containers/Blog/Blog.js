fe362ffe90635c4a593f332e13a92573c48535a9
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-23 22:04:59 +0300

    177 Setting Up the Router Package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,69 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axious from 'axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null
-    }
+  state = {
+    posts: [],
+    selectedPostId: null
+  };
 
-    componentDidMount() {
-        axious.get('http://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                   return {
-                       ...post,
-                       author: 'Piper'
-                   }
-                });
-
-                this.setState({posts: updatedPosts});
-                // console.log(response);
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
-    render () {
-        const posts = this.state.posts.map(post => {
-           return <Post
-               key={post.id}
-               title={post.title}
-               author={post.author}
-               clicked={() => this.postSelectedHandler(post.id)}
-           />;
+  componentDidMount() {
+    axious.get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Piper'
+          };
         });
 
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
-                        </ul>
-                    </nav>
-                </header>
-                <section className="Posts">
-                   {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section className="Posts">
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+        this.setState({posts: updatedPosts});
+        // console.log(response);
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id});
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)}
+      />;
+    });
+
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/new-post">New Post</a></li>
+            </ul>
+          </nav>
+        </header>
+        <section className="Posts">
+          {posts}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
