566bee08e9231e68e703ab671521a1dd2b44f83f
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-10-24 22:48:33 +0300

    193 Navigating Programmatically

--- src/containers/Blog/Posts/index.js
+++ src/containers/Blog/Posts/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios/index';
-import {Link} from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -33,23 +32,21 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
+    this.props.history.push({pathname: '/' + id});
   };
 
 
   render() {
     const posts = this.state.posts.map(post => {
       return (
-        <Link
-          to={'/' + post.id}
-            key={post.id}
-        >
+        //<Link to={'/' + post.id} key={post.id} >
           <Post
+            key={post.id}
             title={post.title}
             author={post.author}
             clicked={() => this.postSelectedHandler(post.id)}
           />
-        </Link>
+        //</Link>
       );
     });
 
