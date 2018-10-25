63dfe46556d6f02374a2c88fefff9c9fb78f3e47
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-10-25 16:33:15 +0300

    185. Navigating Back & To Next Page

--- src/components/Order/CheckoutSummary/index.js
+++ src/components/Order/CheckoutSummary/index.js
@@ -4,7 +4,7 @@ import Burger from '../../Burger';
 import Button from '../../UI/Button';
 import classes from './CheckoutSummary.css';
 
-const CheckoutSummary = ({ ingredients }) => (
+const CheckoutSummary = ({ ingredients, checkoutCancel, checkoutContinue }) => (
     <div className={classes.CheckoutSummary}>
         <h1>We hope it tastes well!</h1>
         <div style={{ width: '100%', margin: 'auto' }}>
@@ -12,10 +12,12 @@ const CheckoutSummary = ({ ingredients }) => (
         </div>
         <Button
             btnType="Danger"
+            click={ checkoutCancel }
         >
             CANCEL</Button>
         <Button
             btnType="Success"
+            click={ checkoutContinue }
         >
             CONTINUE</Button>
     </div>
--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -13,10 +13,22 @@ class Checkout extends Component {
         }
     };
 
+    checkoutCancelHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinueHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancel={this.checkoutCancelHandler}
+                    checkoutContinue={this.checkoutContinueHandler}
+                />
             </div>
         );
     }
