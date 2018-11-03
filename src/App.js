c19cf33dddaa56f07cdc59855994cf60f022ea9e
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-03 12:12:08 +0300

    i Prettier + 214 Adding an Orders Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Layout from './hoc/Layout';
-import BurgerBuilder from './containers/BurgerBuilder';
-import Checkout from './containers/Checkout';
+import Layout from "./hoc/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder";
+import Checkout from "./containers/Checkout";
+import Orders from "./containers/Orders";
 
 class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout}/>
-                        <Route path="/" exact component={BurgerBuilder}/>
-                    </Switch>
-                    {/*<BurgerBuilder/>*/}
-                    {/*<Checkout/>*/}
-                </Layout>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
