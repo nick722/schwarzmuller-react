f496aaaf804d6a5ec144a9d5beea74f3e0fa726a
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-04 18:21:02 +0300

    233 Handling Overall Form Validity

--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Button.css';
+import classes from "./Button.css";
 
-const Button = ({children, click, btnType}) => (
-    <button
-        className={[classes.Button, classes[btnType]].join(' ')}
-        onClick={click}
-    >
-        {children}</button>
+const Button = ({ children, click, btnType, disabled }) => (
+  <button
+    disabled={disabled}
+    className={[classes.Button, classes[btnType]].join(" ")}
+    onClick={click}
+  >
+    {children}
+  </button>
 );
 
 export default Button;
--- src/containers/Checkout/ContactData/index.js
+++ src/containers/Checkout/ContactData/index.js
@@ -84,9 +84,11 @@ class ContactData extends Component {
             { value: "cheapest", displayValue: "Cheapest" }
           ]
         },
-        value: ""
+        value: "",
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   };
 
@@ -149,8 +151,12 @@ class ContactData extends Component {
     );
     updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    console.log("updatedFormElement", updatedFormElement);
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -176,7 +182,9 @@ class ContactData extends Component {
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
+          ORDER
+        </Button>
       </form>
     );
 
