86cb29314f09baadb8534dc1d7b255cf90f28c42
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-05 16:33:19 +0300

    247 Passing and Retrieving Data with Action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,6 +6,13 @@ const reducer = (state = inititalState, action) => {
   switch (action.type) {
     case "INCREMENT":
       return { ...state, counter: state.counter + 1 };
+    case "DECREMENT":
+      return { ...state, counter: state.counter - 1 };
+    case "ADD":
+      return { ...state, counter: state.counter + action.value };
+    case "SUBSTRACT":
+      return { ...state, counter: state.counter - action.value };
+
     default:
       return state;
   }
