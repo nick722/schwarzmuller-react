793717225e6e0cc62ae99d78b34ffbf9130d4850
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-07 23:06:10 +0300

    265 Adjusting Checkout and Contact Data

--- src/containers/Checkout/ContactData/index.js
+++ src/containers/Checkout/ContactData/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Button from "../../../components/UI/Button";
 import classes from "./ContactData.css";
@@ -207,4 +208,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients,
+    price: state.totalPrice
+  };
+};
+
+export default connect(mapStateToProps)(ContactData);
