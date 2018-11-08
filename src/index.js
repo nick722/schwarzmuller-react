4701adc74e7c8f7926ddc94246eb9401c6175a53
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-08 22:14:52 +0300

    270 Using the Redux Devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 import "./index.css";
@@ -26,10 +26,11 @@ const logger = store => {
   };
 };
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger)
+  composeEnhancers(applyMiddleware(logger))
 );
 
 ReactDOM.render(
