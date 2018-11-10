d2b4f260dbbdc8e2f2d79607025b36d5fd5dd0b4
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-10 19:01:49 +0300

    277 Using Action Creators and Get State

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,6 +1,8 @@
 import * as actionTypes from "./actionTypes";
 
 export const saveResult = result => {
+  // const updatedResult = result * 2;
+
   return {
     type: actionTypes.STORE_RESULT,
     result: result
@@ -8,8 +10,10 @@ export const saveResult = result => {
 };
 
 export const storeResult = result => {
-  return dispatch => {
+  return (dispatch, getState) => {
     setTimeout(() => {
+      const oldConter = getState().counter.counter;
+      console.log("oldConter>>>", oldConter);
       dispatch(saveResult(result));
     }, 2000);
   };
