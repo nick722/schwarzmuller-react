99c45f1807a535274a0c58c4c49666cd3962b832
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-11 18:22:29 +0300

    295 Working on Order Actions

--- src/containers/Checkout/ContactData/index.js
+++ src/containers/Checkout/ContactData/index.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
         valid: true
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   };
 
   orderHandler = event => {
@@ -188,7 +187,7 @@ class ContactData extends Component {
       </form>
     );
 
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
 
@@ -204,12 +203,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ingredients: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   };
 };
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: orderData => dispatch(actions.purchaseBurgerStart(orderData));
+  return {
+    onOrderBurger: orderData => dispatch(actions.purchaseBurger(orderData))
+  };
 };
 
 export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
