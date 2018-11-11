caf033cb2915950794c9650c778142c3f12f5e28
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-11 12:24:47 +0300

    288 Executing Asynchronous Code

--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -21,14 +21,14 @@ class BurgerBuilder extends Component {
   componentDidMount() {
     console.log(this.props);
 
-    // axios
-    //   .get("https://yn-react-burger.firebaseio.com/ingredients.json")
-    //   .then(response => {
-    //     this.setState({ ingredients: response.data });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ error: true });
-    //   });
+    axios
+      .get("https://yn-react-burger.firebaseio.com/ingredients.json")
+      .then(response => {
+        this.setState({ ingredients: response.data });
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
   }
 
   updatePurchaseState(ingredients) {
