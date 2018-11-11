1fc78b7c2dde8b5b7705ff0892a53d3236765168
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-11-11 11:34:27 +0300

    285 Installing the Redux Devtools

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://yn-react-burger.firebaseio.com/'
+  baseURL: "https://yn-react-burger.firebaseio.com/"
 });
 
 export default instance;
