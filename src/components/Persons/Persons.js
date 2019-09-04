ea7daa4dcfbab7d425d44f9f4a2706a75d881672
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-09-04 21:53:10 +0300

    7.7. Component Creation Lifecycle in Action

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Person from "./Person/Person";
 
-const Persons = ({ changed, clicked, persons }) =>
-  persons.map((person, index) => (
-    <Person
-      click={() => clicked(index)}
-      name={person.name}
-      age={person.age}
-      change={event => changed(event, person.id)}
-    />
-  ));
+const Persons = ({ changed, clicked, persons }) => {
+  console.log("[Persons.js] rendering...");
+  return persons.map((person, index) => {
+    return (
+      <Person
+        click={() => clicked(index)}
+        name={person.name}
+        age={person.age}
+        change={event => changed(event, person.id)}
+      />
+    );
+  });
+};
 
 export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: 1, name: "Kolya", age: 35 },
@@ -12,6 +17,19 @@ class App extends Component {
     ],
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("[App.js] getDerivedStateFromProps", props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log("[App.js] componentWillMount");
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componenDidMount");
+  }
 
   changeNameHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -46,6 +64,7 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
     const style = {
       backgroundColor: "green",
       color: "white",
